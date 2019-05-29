1387384ed0cc001c76e8bb784ba9c3f262025f9a
Ruslan Freydlin Black_Zerg <blackzerg@gmail.com>
2019-05-29 15:48:18 +0400

    Initial commit

---
+++ app/src/main/java/studio/bz_soft/catviewer/root/BackPressedInterface.kt
@@ -0,0 +1,5 @@
+package studio.bz_soft.catviewer.root
+
+interface BackPressedInterface {
+    fun onBackPressed(): Boolean
+}
---
+++ app/src/main/java/studio/bz_soft/catviewer/root/Constants.kt
@@ -0,0 +1,7 @@
+package studio.bz_soft.catviewer.root
+
+object Constants {
+    const val DB_NAME = "cats.db"
+
+    const val API_MAIN_URL = "https://api.thecatapi.com/v1/"
+}
